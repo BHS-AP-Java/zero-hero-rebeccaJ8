7fdafcb972180b12f932952b40275645ededad00
rebeccaJ8 <2019429@apps.nsd.org>
2025-09-12 16:46:33 +0000

    I am mastering PrintLN I got this!

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,27 @@
-// Student name
-// class period
-// project name
-// date
+// Rebecca Ji
+// P2
+// Zero-Hero
+// 9/12/
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr.Reiber says
+ * INPUT: We don't know about input yet
+ * OUTPUT: Some cool works in the comand line terminal  thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+class Main{
+
+  public static void main(String[] args){
+    System.out.println("---------------------");
+    System.out.println("!!! PROGRAM START !!!");
+    System.out.println("hello");
+    System.out.println("!!! PROGRAM END !!!");
+    System.out.println("---------------------");
+
+
+
+  }
+}
