f8e7feec3259fd671f4ba0c5da6249af50c3bf09
rebeccaJ8 <2019429@apps.nsd.org>
2025-10-28 17:02:29 +0000

    i substituted in the top and leg variables

--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -5,25 +5,40 @@ public class Table {
   int legs;
   int width;
   int height = 8;
+  String top = "=";
+  String leg = "|";
+
 
   public Table(int legs, int width) {
     this.legs = legs;
     this.width = width;
   }
 
+ void setTop(String top){
+      this.top = top;
+
+    }
+
+  void setleg(String leg){
+      this.leg = leg;
+
+    }
+
   public void draw() {
 
+
+
     for (int j = 0; j < width; j++) {
-      System.out.print("=");
+      System.out.print(top);
     }
     System.out.println();
 
        for (int i = 0; i < height; i++) {
-      System.out.print("|");
+      System.out.print(leg);
       for (int s = 0; s < width-2; s++) {
       System.out.print(" ");
       }
-      System.out.println("|");
+      System.out.println(leg);
 
 
     }
