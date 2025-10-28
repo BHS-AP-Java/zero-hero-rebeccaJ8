751165dcb8fcb8fb068bb36c46d2aae058602322
rebeccaJ8 <2019429@apps.nsd.org>
2025-10-28 16:44:25 +0000

    i plugged in variables

--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -2,22 +2,28 @@ package edu.bhscs;
 
 public class Table {
 
-  int seats;
-  double price;
+  int legs;
+  int width;
+  int height = 8;
 
-  public Table(int seats, double price) {
-    this.seats = seats;
-    this.price = price;
+  public Table(int legs, int width) {
+    this.legs = legs;
+    this.width = width;
   }
 
   public void draw() {
 
-    for (int j = 0; j < 20; j++) {
+    for (int j = 0; j < width; j++) {
       System.out.print("=");
     }
     System.out.println();
-    for (int i = 0; i < 8; i++) {
-      System.out.println("|                  |");
+
+       for (int i = 0; i < height; i++) {
+      System.out.print("|");
+      for (int s = 0; s < width-2; s++) {
+      System.out.print(" ");
+      }
+      System.out.println("|");
 
 
     }
